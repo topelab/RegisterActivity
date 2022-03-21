cf8c2e729d36756b0864bfd1dddd99dddf42cc94
Juan López López <detoslaos@outlook.com>
2022-03-21 22:19:01 +0100

    Creando RegisterActivities

---
+++ src/Tools/RegisterActivity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("Register activities\nPress <Esc> to exit");
+bool exit = false;
+while (!exit)
+{
+    var key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Escape)
+    {
+        exit = true;
+    }
+}
