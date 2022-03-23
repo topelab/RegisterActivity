b9eddf349ac9bd16396dc406667786a95b63eb1a
Juan López López <detoslaos@outlook.com>
2022-03-23 07:02:03 +0100

    Capturando processes en RegisterActivity

--- src/Tools/RegisterActivity/Program.cs
+++ src/Tools/RegisterActivity/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
-Console.WriteLine("Register activities\nPress <Esc> to exit");
-bool exit = false;
-while (!exit)
+namespace RegisterActivity
 {
-    var key = Console.ReadKey();
-    if (key.Key == ConsoleKey.Escape)
+    internal class Program
     {
-        exit = true;
+        private static Timer timer;
+        private static bool stopNow;
+
+        private static void Main(string[] args)
+        {
+            Prepare();
+            Console.WriteLine("Register activities\nPress <Esc> to exit");
+            bool exit = false;
+            while (!exit)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    exit = true;
+                    stopNow = true;
+                    timer.Dispose();
+                }
+            }
+        }
+
+        static void Prepare()
+        {
+            stopNow = false;
+            timer = new Timer(CheckProcesses, stopNow, 0, 5000);
+        }
+
+        private static void CheckProcesses(object state)
+        {
+            var processes = Process.GetProcesses().Where(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle)).Select(p => new { p.MainWindowTitle, p.ProcessName, p.StartTime, p.MainModule.FileName });
+            Console.WriteLine(JsonConvert.SerializeObject(processes, Formatting.None));
+        }
     }
 }
