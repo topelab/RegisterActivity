1a904637cb4a11848e439eff3676523921820236
Juan López López <detoslaos@outlook.com>
2022-03-25 20:16:03 +0100

    Creando ProcessService para recolectar procesos

---
+++ src/Tools/RegisterActivity/ProcessDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RegisterActivity
+{
+    internal class ProcessDTO
+    {
+        public ProcessDTO(string mainWindowTitle, string processName, DateTime startTime, string fileName)
+        {
+            MainWindowTitle = mainWindowTitle;
+            ProcessName = processName;
+            StartTime = startTime;
+            FileName = fileName;
+        }
+
+        public string MainWindowTitle { get; set; }
+        public string ProcessName { get; set; }
+        public DateTime StartTime { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ src/Tools/RegisterActivity/ProcessService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Timers;
+
+namespace RegisterActivity
+{
+    internal class ProcessService
+    {
+        private readonly Timer timer;
+        private Action<IEnumerable<ProcessDTO>> onNewProcesses;
+
+        public ProcessService()
+        {
+            timer = new Timer();
+            timer.Elapsed += OnTimerElapsed;
+        }
+
+        public void Start(Action<IEnumerable<ProcessDTO>> onNewProcesses, double interval = 5000)
+        {
+            timer.Interval = interval;
+            this.onNewProcesses = onNewProcesses;
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            IEnumerable<ProcessDTO> processes = Process.GetProcesses()
+                .Where(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle))
+                .Select(p => new ProcessDTO(p.MainWindowTitle, p.ProcessName, p.StartTime, p.MainModule.FileName));
+
+            onNewProcesses?.Invoke(processes);
+        }
+    }
+}
