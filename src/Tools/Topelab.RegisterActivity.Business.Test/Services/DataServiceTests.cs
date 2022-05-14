a4a45c16935c72f72511196fca48bab1e449edcc
Juan López López <detoslaos@outlook.com>
2022-05-14 13:18:31 +0200

    Adding test projoct

---
+++ src/Tools/Topelab.RegisterActivity.Business.Test/Services/DataServiceTests.cs
@@ -0,0 +1,66 @@
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Topelab.RegisterActivity.Business.DTO;
+using Topelab.RegisterActivity.Business.Services;
+using Topelab.RegisterActivity.Business.Services.Entities;
+using Topelab.RegisterActivity.Domain.Entities;
+
+namespace Topelab.RegisterActivity.Business.Test.Services
+{
+    [TestFixture]
+    public class DataServiceTests
+    {
+        private MockRepository mockRepository;
+        private Mock<IWinlogService> mockWinlogService;
+        private Stack<Winlog> mockWinlogStack;
+        private static int id = 0;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockWinlogStack = new Stack<Winlog>();
+
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockWinlogService = this.mockRepository.Create<IWinlogService>();
+            this.mockWinlogService
+                .Setup(m => m.Save(It.IsAny<Winlog>()))
+                .Returns((Winlog w) =>
+                {
+                    w.LocalId = ++id;
+                    mockWinlogStack.Push(w);
+                    return id;
+                });
+        }
+
+        private DataService CreateService()
+        {
+            return new DataService(
+                this.mockWinlogService.Object);
+        }
+
+        [Test]
+        public void CalculateData_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var service = this.CreateService();
+            var process = Process.GetCurrentProcess();
+            var lastTimeActive = DateTime.Now.AddMinutes(-10);
+            ProcessDTO currentProcess = new(process, "DataServiceTest") { LastTimeActive = lastTimeActive };
+            ProcessDTO currentProcess2 = new(process, "DataServiceTest2") { LastTimeActive = lastTimeActive };
+            
+            Action<ProcessDTO> afterSave = null;
+
+            // Act
+            service.CalculateData(currentProcess, afterSave);
+            service.CalculateData(currentProcess2, afterSave);
+
+            // Assert
+            Assert.IsTrue(mockWinlogStack.Peek().TotalTime > 0);
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
